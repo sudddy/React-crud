86f0150bdfca7d17968ec63a5fe608d34d875b87
Sudharsan Srinivasan <sudharsan_tw@twadmins-iMac.local>
2020-03-12 11:21:20 +1300

    creating Forms for adding the users

--- basic-crud/src/App.js
+++ basic-crud/src/App.js
@@ -3,15 +3,20 @@ import UserTable from './component/UserTable/userTable';
 import {Col, Row} from 'react-bootstrap'
 
 const App = () => {
-  
+
   const usersData = [
     {id: 1, name: 'Sudharsan', username: 'suddy'},
     {id: 2, name: 'raghavan', username: 'rags'},
     {id: 3, name: 'kaushik', username: 'cow'},
     {id: 4, name: 'chandru', username: 'chandy'},
   ]
-
   const [users, setUsers] = useState(usersData);
+
+  const addUser = user => {
+    user.id = user.length + 1;
+    setUsers([...users,user]);
+  }
+  
   return (
     <div className="container">
       <h1>Basic Crud operations in React</h1>
